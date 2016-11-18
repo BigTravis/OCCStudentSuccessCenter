4d33b21c1d45dfe4294119b8ef338b8f20538792
ischenck <ianschenck123@gmail.com>
2016-11-17 16:52:17 -0800

    updated database

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/DBHelper.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/DBHelper.java
@@ -24,22 +24,16 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String FIELD_LAST_NAME = "last_name";
     private static final String FIELD_FIRST_NAME = "first_name";
 
-    private static final String DAYS_TABLE = "Days";
-    private static final String DAYS_KEY_FIELD_ID = "id";
-    private static final String FIELD_DAY_NAME = "day_name";
-
     private static final String TIMES_TABLE = "Times";
     private static final String TIMES_KEY_FIELD_ID = "id";
-    private static final String FIELD_TIME = "time";
+    private static final String FIELD_DAY = "day";
+    private static final String FIELD_HALF_HOUR = "half_hour";
 
-    private static final String TUTOR_COURSE_TABLE = "TutorCourseRelationship";
+    private static final String TUTOR_TIME_TABLE = "TutorAvailability";
+    private static final String FIELD_TUTOR_ID = "tutor_id";
     private static final String FIELD_COURSE_ID = "course_id";
-    private static final String FIELD_TUTOR_ID_COURSE_R = "tutor_id";
-
-    private static final String TUTOR_TIME_TABLE = "TutorTimeRelationship";
-    private static final String FIELD_TUTOR_ID_TIME_R = "tutor_id";
-    private static final String FIELD_DAY_ID = "day_id";
-    private static final String FIELD_TIME_ID = "time_id";
+    private static final String FIELD_START_TIME_ID = "start_time_id";
+    private static final String FIELD_END_TIME_ID = "end_time_id";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -58,35 +52,24 @@ public class DBHelper extends SQLiteOpenHelper {
                 + FIELD_LAST_NAME + " TEXT, " + FIELD_FIRST_NAME + " TEXT" + ")";
         database.execSQL(table);
 
-        table = "CREATE TABLE " + DAYS_TABLE + "("
-                + DAYS_KEY_FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + FIELD_DAY_NAME + " TEXT" + ")";
-        database.execSQL(table);
 
         table = "CREATE TABLE " + TIMES_TABLE + "("
                 + TIMES_KEY_FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + FIELD_TIME + " TEXT" + ")";
-        database.execSQL(table);
-
-        table = "CREATE TABLE " + TUTOR_COURSE_TABLE + "("
-                + FIELD_COURSE_ID + " INTEGER, "
-                + FIELD_TUTOR_ID_COURSE_R + " INTEGER, "
-                + "FOREIGN KEY(" + FIELD_COURSE_ID + ") REFERENCES "
-                + COURSES_TABLE + "(" + COURSES_KEY_FIELD_ID + "), "
-                + "FOREIGN KEY(" + FIELD_TUTOR_ID_COURSE_R + ") REFERENCES "
-                + TUTORS_TABLE + "(" + TUTORS_KEY_FIELD_ID + ")"
-                + ")";
+                + FIELD_DAY + " TEXT" + FIELD_HALF_HOUR + " INTEGER" + ")";
         database.execSQL(table);
 
         table = "CREATE TABLE " + TUTOR_TIME_TABLE + "("
-                + FIELD_DAY_ID + " INTEGER, "
-                + FIELD_TIME_ID + " INTEGER, "
-                + FIELD_TUTOR_ID_TIME_R + " INTEGER, "
-                + "FOREIGN KEY(" + FIELD_DAY_ID + ") REFERENCES "
-                + DAYS_TABLE + "(" + DAYS_KEY_FIELD_ID + "), "
-                + "FOREIGN KEY(" + FIELD_TIME_ID + ") REFERENCES "
+                + FIELD_START_TIME_ID + " INTEGER, "
+                + FIELD_END_TIME_ID + " INTEGER, "
+                + FIELD_TUTOR_ID + " INTEGER, "
+                + FIELD_COURSE_ID + " TEXT, "
+                + "FOREIGN KEY(" + FIELD_START_TIME_ID + ") REFERENCES "
                 + TIMES_TABLE + "(" + TIMES_KEY_FIELD_ID + "), "
-                + "FOREIGN KEY(" + FIELD_TUTOR_ID_TIME_R + ") REFERENCES "
+                + "FOREIGN KEY(" + FIELD_END_TIME_ID + ") REFERENCES "
+                + TIMES_TABLE + "(" + TIMES_KEY_FIELD_ID + "), "
+                + "FOREIGN KEY(" + FIELD_COURSE_ID + ") REFERENCES "
+                + COURSES_TABLE + "(" + FIELD_COURSE_ID + "), "
+                + "FOREIGN KEY(" + FIELD_TUTOR_ID + ") REFERENCES "
                 + TUTORS_TABLE + "(" + TUTORS_KEY_FIELD_ID + ")"
                 + ")";
         database.execSQL(table);
@@ -97,10 +80,8 @@ public class DBHelper extends SQLiteOpenHelper {
     {
         database.execSQL("DROP TABLE IF EXISTS " + COURSES_TABLE);
         database.execSQL("DROP TABLE IF EXISTS " + TUTORS_TABLE);
-        database.execSQL("DROP TABLE IF EXISTS " + DAYS_TABLE);
         database.execSQL("DROP TABLE IF EXISTS " + TIMES_TABLE);
         database.execSQL("DROP TABLE IF EXISTS " + TUTORS_TABLE);
-        database.execSQL("DROP TABLE IF EXISTS " + TUTOR_COURSE_TABLE);
         database.execSQL("DROP TABLE IF EXISTS " + TUTOR_TIME_TABLE);
 
         onCreate(database);
