6a3a91b9703cddde3a16fe847e10dddd26c25d2f
tmorrissey1 <tmorrissey1@student.cccd.edu>
2016-11-22 16:16:39 -0800

    Removed ArrayList member variables in Tutor class

---
+++ app/src/androidTest/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/TutorTest.java
@@ -0,0 +1,43 @@
+package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by tmorrissey1 on 11/22/2016.
+ */
+public class TutorTest {
+    private Tutor mTestTutor;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestTutor = new Tutor();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void getLastName() throws Exception {
+
+    }
+
+    @Test
+    public void describeContents() throws Exception {
+
+    }
+
+    @Test
+    public void writeToParcel() throws Exception {
+
+    }
+
+}
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/Tutor.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/Tutor.java
@@ -3,9 +3,6 @@ package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsu
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Represents a Tutor at Orange Coast Community College
  * Created by tmorrissey1 on 11/15/2016.
@@ -15,8 +12,7 @@ public class Tutor implements Parcelable{
     private int id;
     private String firstName;
     private String lastName;
-    private List<Course> courseList;
-    private List<DayTime> dayTimeList;
+
 
     /**
      * Constructor.
@@ -28,33 +24,18 @@ public class Tutor implements Parcelable{
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-        courseList = new ArrayList();
-        dayTimeList = new ArrayList();
     }
 
-    /**
-     * Constructor.
-     * @param id int The unique id of the Tutor.
-     * @param firstName String The first name of the Tutor
-     * @param lastName String The last name of the Tutor.
-     * @param courseList List<Course> List of Courses that the Tutor tutors.
-     * @param dayTimeList List<DayTime> List of DayTimes that the Tutor is available.
-     */
-    public Tutor(int id, String firstName, String lastName, List<Course> courseList,
-                 List<DayTime> dayTimeList) {
-        this.id = id;
+    public Tutor(String firstName, String lastName) {
+        id = -1;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.courseList = courseList;
-        this.dayTimeList = dayTimeList;
     }
 
     protected Tutor(Parcel in) {
         id = in.readInt();
         firstName = in.readString();
         lastName = in.readString();
-        courseList = in.createTypedArrayList(Course.CREATOR);
-        dayTimeList = in.createTypedArrayList(DayTime.CREATOR);
     }
 
     public static final Creator<Tutor> CREATOR = new Creator<Tutor>() {
@@ -101,38 +82,6 @@ public class Tutor implements Parcelable{
         this.lastName = lastName;
     }
 
-    /**
-     * Gets the list of courses that this Tutor can tutor.
-     * @return List<Course> list of courses.
-     */
-    public List<Course> getCourseList() {
-        return courseList;
-    }
-
-    /**
-     * Sets the course list of this Tutor.
-     * @param courseList List<Course> list of Courses.
-     */
-    public void setCourseList(List<Course> courseList) {
-        this.courseList = courseList;
-    }
-
-    /**
-     * Gets the list of day/times that this Tutor can tutor.
-     * @return List<DayTime> list of day/times.
-     */
-    public List<DayTime> getDayTimeList() {
-        return dayTimeList;
-    }
-
-    /**
-     * Sets the list of day/times that this Tutor can tutor.
-     * @param dayTimeList list of day/times.
-     */
-    public void setDayTimeList(List<DayTime> dayTimeList) {
-        this.dayTimeList = dayTimeList;
-    }
-
     @Override
     public int describeContents() {
         return 0;
@@ -143,7 +92,5 @@ public class Tutor implements Parcelable{
         parcel.writeInt(id);
         parcel.writeString(firstName);
         parcel.writeString(lastName);
-        parcel.writeTypedList(courseList);
-        parcel.writeTypedList(dayTimeList);
     }
 }
