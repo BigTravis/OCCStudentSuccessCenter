0237d3f28653f6ba8a248e3be6eb04ebbfeb1163
Keegan Frederick <tefsuncle@hotmail.com>
2016-11-29 13:54:48 -0800

    Began working on connecting a list adapter in the FAQ activity to a string array.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/FAQActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/FAQActivity.java
@@ -1,13 +1,17 @@
 package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class FAQActivity extends AppCompatActivity {
+public class FAQActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,R.array.faq_list))
     }
 }
