38a53bbab957419e6282b778f9d96f18558c433b
kfrederick5 <tefsuncle@gmail.com>
2016-11-29 16:48:58 -0800

    Added Study Group and got a bit further with FAQ activities.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/FAQActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/FAQActivity.java
@@ -1,17 +1,29 @@
 package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
 
 import android.app.ListActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class FAQActivity extends ListActivity {
+
+    private List<String> faq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
 
-        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,R.array.faq_list))
+        setListAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.faq_list)));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int pos, long id)
+    {
+
     }
 }
