db17786999f00b588685d8c93d078cfc14022c80
Ian <ianschenck123@gmail.com>
2016-12-03 20:42:36 -0800

    Made Study Group table and all DB functions for it made a function in the DayTime class that will convert from the float time to a string. updated the tutor details relationship to allow for multiple times. made studyGroupActivity, studyGroupAdapter, activity_study_group, and study_group_item

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/StudyGroupActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/StudyGroupActivity.java
@@ -1,13 +1,27 @@
 package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StudyGroupActivity extends AppCompatActivity {
+
+    private StudyGroupAdapter mStudyGroupAdapter;
+    private ListView mStudyGroupListView;
+    private DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_group);
+        db = new DBHelper(this);
+        ArrayList<StudyGroup> studyGroups = db.getAllStudyGroups();
+
+        mStudyGroupAdapter = new StudyGroupAdapter(this, R.layout.study_group_item, studyGroups);
+        mStudyGroupListView = (ListView) findViewById(R.id.studyGroupListView);
+        mStudyGroupListView.setAdapter(mStudyGroupAdapter);
+
     }
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/StudyGroupAdapter.java
@@ -0,0 +1,64 @@
+package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ian on 12/3/2016.
+ */
+
+public class StudyGroupAdapter extends ArrayAdapter<StudyGroup>  {
+
+    private Context mContext;
+    private List<StudyGroup> mStudyGroupList = new ArrayList<>();
+    private int mResourceId;
+
+    public StudyGroupAdapter(Context context, int resourceId, List<StudyGroup> studyGroups) {
+        super(context, resourceId, studyGroups);
+        mContext = context;
+        mStudyGroupList = studyGroups;
+        mResourceId = resourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final StudyGroup selectedStudyGroup = mStudyGroupList.get(position);
+
+        LayoutInflater inflater =
+                (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(mResourceId, null);
+
+        LinearLayout studyGroupLinearLayout =
+                (LinearLayout) view.findViewById(R.id.studyGroupLinearLayout);
+        TextView courseTextView = (TextView) view.findViewById(R.id.courseTextView);
+        TextView timeTextView = (TextView) view.findViewById(R.id.timeTextView);
+        TextView instructorTextView = (TextView) view.findViewById(R.id.instructorTextView);
+        TextView roomTextView = (TextView) view.findViewById(R.id.roomTextView);
+
+        studyGroupLinearLayout.setTag(selectedStudyGroup);
+
+        courseTextView.setText(selectedStudyGroup.getCourse().toString());
+
+        String timeText = selectedStudyGroup.getDayTime().getDay() + " " + selectedStudyGroup.getDayTime().convertFloatTimeToString();
+        timeTextView.setText(timeText);
+
+        instructorTextView.setText(selectedStudyGroup.getInstructor());
+
+        roomTextView.setText("Room " + selectedStudyGroup.getRoom());
+
+        return view;
+
+
+    }
+}
