f9b9d77b2f71a781901b008d0d80429406fce2ff
Keegan Frederick <tefsuncle@hotmail.com>
2016-12-03 23:51:51 -0800

    Fixed a conflict issue with the FAQ list adapter that was causing the program to crash.

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/FaqListAdapter.java
@@ -0,0 +1,47 @@
+package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Link on 12/3/2016.
+ */
+
+public class FaqListAdapter extends ArrayAdapter<String> {
+    private Context mContext;
+    private List<String> mFaqList = new ArrayList<>();
+    private int mResourceId;
+
+    public FaqListAdapter(Context context, int resourceId, List<String> questionList) {
+        super(context, resourceId, questionList);
+        mContext = context;
+        mFaqList = questionList;
+        mResourceId = resourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final String question = mFaqList.get(position);
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(mResourceId, null);
+
+        LinearLayout faqLinearLayout = (LinearLayout) view.findViewById(R.id.faqLinearLayout);
+        TextView faqTextView = (TextView) view.findViewById(R.id.faqTextView);
+        faqTextView.setText(mFaqList.get(position));
+
+        faqLinearLayout.setTag(question);
+
+        return view;
+    }
+
+}
