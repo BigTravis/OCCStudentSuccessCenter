3c5608401f26f96efb877b758f826e0c09c50912
Ian <ianschenck123@gmail.com>
2016-12-05 15:55:15 -0800

    changed top text to white, rest to blue.  implemented gradient background changed button in tutor details to new style changed tutor search to show the day/time properly changed tutor list to show day tutor is availabe in ListView updated csvs to include all tutors/times for ACCT, ALH, ANTH, BIO, BUS changed search algorithm to allow for day search without time made the maps activity, but I cant figure out how to test it on my emulator made contact activity with direct link to phone (with permissions in place) and link to the location activity

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/ContactActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/ContactActivity.java
@@ -1,13 +1,51 @@
 package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsuccesscenter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private static final int CALL_PHONE_REQUEST_CODE = 111;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        if (ActivityCompat.checkSelfPermission(getBaseContext(),
+                android.Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.CALL_PHONE},
+                    CALL_PHONE_REQUEST_CODE);
+            return;
+        }
+
+
+        findViewById(R.id.phoneNumberTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getBaseContext(),
+                        android.Manifest.permission.CALL_PHONE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.fromParts("tel", "7144325559", null));
+                    startActivity(callIntent);
+                }
+            }
+        });
+
+        findViewById(R.id.addressTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ContactActivity.this, LocationsListActivity.class));
+            }
+        });
     }
 }
