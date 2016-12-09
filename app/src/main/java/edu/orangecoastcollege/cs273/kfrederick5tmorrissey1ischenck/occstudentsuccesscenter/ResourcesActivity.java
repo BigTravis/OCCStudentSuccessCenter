b6e365cf63f8813d81c52e815cb83e80bd1d5763
kfrederick5 <tefsuncle@gmail.com>
2016-12-08 16:54:18 -0800

    Updated profiles.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/ResourcesActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5tmorrissey1ischenck/occstudentsuccesscenter/ResourcesActivity.java
@@ -2,16 +2,17 @@ package edu.orangecoastcollege.cs273.kfrederick5tmorrissey1ischenck.occstudentsu
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
-public class ResourcesActivity extends AppCompatActivity {
+public class ResourcesActivity extends NavDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resources);
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.contentFrame);
+        getLayoutInflater().inflate(R.layout.activity_resources, contentFrameLayout);
     }
 
     public void blackboardOnClick(View v)
